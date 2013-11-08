29ed88c6aab64280ddbe81f61551c5b3892a08a8
Stephane Liu <sliu@sjliu.com>
2013-11-08 13:13:48 -1000

    Add version to init

--- lib/password_list_generator.rb
+++ lib/password_list_generator.rb
@@ -1,3 +1,4 @@
 require          'securerandom' 
+require_relative 'password_list_generator/version'
 require_relative 'password_list_generator/password' 
 require_relative 'password_list_generator/generator' 
